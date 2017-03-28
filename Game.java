a3bd3d41905c50d7923563b73adaafddc6e11d16
Joshua Wagner <tlrwgnr@gmail.com>
2017-03-28 18:44:08 -0400

    added in basic ability to write to txt file and read from it

--- Game.java
+++ Game.java
@@ -1,9 +1,7 @@
 import java.util.*;
-// Immutable.  Stores 3 strings referred to as entity, relation, and
-// property. Each Record has a unique integer ID which is set on
-// creation.  All records are made through the factory method
-// Record.makeRecord(e,r,p).  Record which have some fields wild are
-// created using Record.makeQuery(wild,e,r,p)
+import java.io.PrintWriter;
+import java.io.FileReader;
+
 public class Game {
 
     public int currentPlayer;
@@ -11,22 +9,53 @@ public class Game {
     public boolean isAi;
 
     public Game(){
+        this.isAi = false;
+    }
 
+    public Game(boolean usesAI){
+        if(usesAI){
+            this.isAi = true;
+            this.ai_player = new AI();
+        }
+        else{
+            this.isAi = false;
+        }
     }
 
     public boolean hasWon(){
         return true;
     }
 
-    public boolean saveGame(){
+    public boolean saveGame(String saveString){
+        try{
+            PrintWriter writer = new PrintWriter("saveGame.txt", "UTF-8");
+            writer.println(saveString);
+            writer.close();
+            return true;
+            }
+        catch (Exception e) {
+            System.out.println("There was an error saving the game.");
+            System.out.println(e);
+            }
         return true;
     }
 
     public boolean loadGame(){
-        return true;
+        try{
+            Scanner in = new Scanner(new FileReader("saveGame.txt"));
+            // System.out.println(in.nextLine());
+            return true;
+        }
+        catch(Exception e){
+            System.out.println("There was an error loading the game save.");
+            System.out.println(e);
+        }
+        return false;
     }
 
-    public static void main(String[] args) {
-
-    }
+    // public static void main(String[] args) {
+    //     Game g = new Game();
+    //     g.saveGame("test save");
+    //     g.loadGame();
+    // }
 }
