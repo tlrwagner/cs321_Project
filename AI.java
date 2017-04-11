eaabe0bec5164977937f815ceac17587cdefb8e8
Joseph Bessette <bessettejoey@comcast.net>
2017-04-10 20:09:12 -0400

    continued progress on AI

--- AI.java
+++ AI.java
@@ -1,4 +1,3 @@
-package cs321;
 
 /*
  * This class encompasses the moves that the computer 
@@ -110,6 +109,62 @@ public class AI {
 		}
 		if (this.difficulty == 0){
 			// loop through minCross to figure which tile is empty
+			if(minCross == 0){
+				for(int i = 0; i < 3; i++){
+					if(board[(boardNum * 9) + i] == 0){
+						result = (boardNum * 9) + i;
+					}
+				}
+			}
+			if(minCross == 1){
+				for(int i = 3; i < 6; i++){
+					if(board[(boardNum * 9) + i] == 0){
+						result = (boardNum * 9) + i;
+					}
+				}
+			}
+			if(minCross == 2){
+				for(int i = 7; i < 10; i++){
+					if(board[(boardNum * 9) + i] == 0){
+						result = (boardNum * 9) + i;
+					}
+				}
+			}
+			if(minCross == 3){
+				for(int i = 0; i < 7; i+=3){
+					if(board[(boardNum * 9) + i] == 0){
+						result = (boardNum * 9) + i;
+					}
+				}
+			}
+			if(minCross == 4){
+				for(int i = 1; i < 8; i+=3){
+					if(board[(boardNum * 9) + i] == 0){
+						result = (boardNum * 9) + i;
+					}
+				}
+			}
+			if(minCross == 5){
+				for(int i = 2; i < 9; i+=3){
+					if(board[(boardNum * 9) + i] == 0){
+						result = (boardNum * 9) + i;
+					}
+				}
+			}
+			if(minCross == 6){
+				for(int i = 0; i < 9; i+=4){
+					if(board[(boardNum * 9) + i] == 0){
+						result = (boardNum * 9) + i;
+					}
+				}
+			}
+			if(minCross == 7){
+				for(int i = 6; i > 1; i-=2){
+					if(board[(boardNum * 9) + i] == 0){
+						result = (boardNum * 9) + i;
+					}
+				}
+			}
 		}
 		else{
 			// loop through maxCross to figure which tile is empty
