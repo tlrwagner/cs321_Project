c98b282ec47ef166ce3dab9b899aeb1972b267ce
Joshua Wagner <tlrwgnr@gmail.com>
2017-04-17 08:27:41 -0400

    fixed game to work with boolean arrays

--- Game.java
+++ Game.java
@@ -4,109 +4,113 @@ import java.io.FileReader;
 
 public class Game {
 
-    public int currentPlayer;
-    public AI ai_player;
-    public boolean isAi;
+public int currentPlayer;
+public AI ai_player;
+public boolean isAi;
 
-    /**
-     * Constructor for Game object
-     * @return void
-     */
-    public Game(){
+/**
+ * Constructor for Game object
+ * @return void
+ */
+public Game(){
         this.isAi = false;
-    }
+}
 
-    /**
-     * Constructor for Game object
-     * @param  boolean usesAI        instance variable denoting if an AI object is being used
-     * @return         void
-     */
-    public Game(boolean usesAI){
-        if(usesAI){
-            this.isAi = true;
-            this.ai_player = new AI();
+/**
+ * Constructor for Game object
+ * @param  boolean usesAI        instance variable denoting if an AI object is being used
+ * @return         void
+ */
+public Game(boolean usesAI){
+        if(usesAI) {
+                this.isAi = true;
+                this.ai_player = new AI(0);
         }
         else{
-            this.isAi = false;
+                this.isAi = false;
         }
-    }
+}
 
-    /**
-     * if a player has won
-     * @return if function ran successfully
-     */
-    public boolean hasWon(String[] board){
+/**
+ * if a player has won
+ * @return if function ran successfully
+ */
+public boolean hasWon(boolean[] board){
         strArr = Array.asList(board);
         if(
-        strArr.contains("0") && strArr.contains("1") && strArr.contains("2") ||
-        strArr.contains("3") && strArr.contains("4") && strArr.contains("5") ||
-        strArr.contains("6") && strArr.contains("7") && strArr.contains("8") ||
-        strArr.contains("0") && strArr.contains("3") && strArr.contains("6") ||
-        strArr.contains("1") && strArr.contains("4") && strArr.contains("7") ||
-        strArr.contains("2") && strArr.contains("5") && strArr.contains("8") ||
-        strArr.contains("0") && strArr.contains("4") && strArr.contains("8") ||
-        strArr.contains("2") && strArr.contains("4") && strArr.contains("6") ||
+                strArr[0] && strArr[1] && strArr[2] ||
+                strArr[3] && strArr[4] && strArr[5] ||
+                strArr[6] && strArr[7] && strArr[8] ||
+                strArr[0] && strArr[3] && strArr[6] ||
+                strArr[1] && strArr[4] && strArr[7] ||
+                strArr[2] && strArr[5] && strArr[8] ||
+                strArr[0] && strArr[4] && strArr[8] ||
+                strArr[2] && strArr[4] && strArr[6] ||
 
-        strArr.contains("9") && strArr.contains("10") && strArr.contains("11") ||
-        strArr.contains("12") && strArr.contains("13") && strArr.contains("14") ||
-        strArr.contains("15") && strArr.contains("16") && strArr.contains("17") ||
-        strArr.contains("9") && strArr.contains("12") && strArr.contains("15") ||
-        strArr.contains("10") && strArr.contains("13") && strArr.contains("16") ||
-        strArr.contains("11") && strArr.contains("14") && strArr.contains("17") ||
-        strArr.contains("0") && strArr.contains("13") && strArr.contains("17") ||
-        strArr.contains("11") && strArr.contains("13") && strArr.contains("15") ||
+                strArr.[9] && strArr.[10]) && strArr.[11]) ||
+                strArr.[12]) && strArr.[13]) && strArr.[14]) ||
+                strArr.[15]) && strArr.[16]) && strArr.[17]) ||
+                strArr.[9] && strArr.[12]) && strArr.[15]) ||
+                strArr.[10]) && strArr.[13]) && strArr.[16]) ||
+                strArr.[11]) && strArr.[14]) && strArr.[17]) ||
+                strArr.[0] && strArr.[13]) && strArr.[17]) ||
+                strArr.[11]) && strArr.[13]) && strArr.[15]) ||
 
-        strArr.contains("18") && strArr.contains("19") && strArr.contains("20") ||
-        strArr.contains("21") && strArr.contains("22") && strArr.contains("23") ||
-        strArr.contains("24") && strArr.contains("25") && strArr.contains("26") ||
-        strArr.contains("18") && strArr.contains("21") && strArr.contains("22") ||
-        strArr.contains("19") && strArr.contains("22") && strArr.contains("25") ||
-        strArr.contains("20") && strArr.contains("23") && strArr.contains("26") ||
-        strArr.contains("18") && strArr.contains("22") && strArr.contains("26") ||
-        strArr.contains("20") && strArr.contains("22") && strArr.contains("24") ||
-        ){
-            return true;
+                strArr.[18] && strArr.[19] && strArr.[20] ||
+                strArr.[21] && strArr.[22] && strArr.[23] ||
+                strArr.[24] && strArr.[25] && strArr.[26] ||
+                strArr.[18] && strArr.[21] && strArr.[22] ||
+                strArr.[19] && strArr.[22] && strArr.[25] ||
+                strArr.[20] && strArr.[23] && strArr.[26] ||
+                strArr.[18] && strArr.[22] && strArr.[26] ||
+                strArr.[20] && strArr.[22] && strArr.[24] ||
+                ) {
+                return true;
         }
 
         return false;
-    }
+}
 
-    /**
-     * Saves game, writes board status to saveGame.txt as a String
-     * @param  String saveString    input String from GUI object representing the board
-     * @return        if function ran successfully
-     */
-    public boolean saveGame(String[] saveArray){
+/**
+ * Saves game, writes board status to saveGame.txt as a String
+ * @param  String saveString    input String from GUI object representing the board
+ * @return        if function ran successfully
+ */
+public boolean saveGame(boolean[] saveArray){
         String saveString = "";
-        for(item : saveArray){
-            saveString += item;
+        for(item : saveArray) {
+                saveString += item + " ";
         }
         try{
-            PrintWriter writer = new PrintWriter("saveGame.txt", "UTF-8");
-            writer.println(saveString);
-            writer.close();
-            return true;
-            }
+                PrintWriter writer = new PrintWriter("saveGame.txt", "UTF-8");
+                writer.println(saveString);
+                writer.close();
+                return true;
+        }
         catch (Exception e) {
-            System.out.println("There was an error saving the game.");
-            System.out.println(e);
-            }
+                System.out.println("There was an error saving the game.");
+                System.out.println(e);
+        }
         return false;
-    }
+}
 
-    /**
-     * loads String from saveGame.txt
-     * @return if function ran successfully
-     */
-    public String[] loadGame(){
+/**
+ * loads String from saveGame.txt
+ * @return if function ran successfully
+ */
+public String[] loadGame(){
+        result = String[26];
         try{
-            Scanner in = new Scanner(new FileReader("saveGame.txt"));
-            return true;
+                Scanner in = new Scanner(new FileReader("saveGame.txt"));
+                while(in.hasNext()) {
+                        result.append(in.next());
+                }
+                return true;
         }
-        catch(Exception e){
-            System.out.println("There was an error loading the game save.");
-            System.out.println(e);
+        catch(Exception e) {
+                System.out.println("There was an error loading the game save.");
+                System.out.println(e);
         }
         return false;
-    }
+}
 }
--- ThreeXT_main.java
+++ ThreeXT_main.java
@@ -2,23 +2,28 @@
 import java.util.*;
 
 public class ThreeXT_main {
-    public Game game;
-    public GUI gui;
+public Game game;
+public GUI gui;
+public String lastTurn;
 
-    public ThreeXT_main(){
+public ThreeXT_main(){
         this.game = new Game();
         this.gui = new GUI();
-    }
 
-    /**
-     * runs flow for game object
-     * @return if function ran successfully
-     */
-    public boolean runGame(){
+        // while(!game.hasWon(gui.getBoard())){
+        //
+        // }
+}
+
+/**
+ * runs flow for game object
+ * @return if function ran successfully
+ */
+public boolean runGame(){
         return true;
-    }
+}
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
 
-    }
+}
 }
